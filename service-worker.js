ee023d2f7540008459055b3a9ac6868db346fe03
karch1 <rkdeotjdak@naver.com>
2026-05-26 16:40:15 +0900

    Update service-worker.js
    서비스워크 업데이트 목록 추가

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "db-manager-v1";
+const CACHE_NAME = "db-manager-v2"; // v1 -> v2로 변경
 
 const urlsToCache = [
     "./",
@@ -6,24 +6,33 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
-
     event.waitUntil(
-
         caches.open(CACHE_NAME)
             .then((cache) => {
-
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
+// 중요: 새로운 서비스 워커가 활성화될 때 이전 캐시를 삭제하도록 추가
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener("fetch", (event) => {
-
     event.respondWith(
-
         caches.match(event.request)
             .then((response) => {
-
                 return response || fetch(event.request);
             })
     );
